0f73424ace53710a8d336707e5ea21446c69a2cd
RichardJanebo <richardjanebo@gmail.com>
2025-04-25 20:35:06 -0300

    feat:add spring in this project

---
+++ src/main/java/com/vamospassar/respostabot/RespostabotApplication.java
@@ -0,0 +1,13 @@
+package com.vamospassar.respostabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespostabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespostabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vamospassar/respostabot/RespostabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamospassar.respostabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespostabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
